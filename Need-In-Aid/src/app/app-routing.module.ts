ed79e73d8f7541d08e73338eb0a2ab38ffc92e40
1804054Miraz <u1804054@student.cuet.ac.bd>
2023-11-12 23:26:06 +0600

    provide Homepage structure and decorte it

--- Need-In-Aid/src/app/app-routing.module.ts
+++ Need-In-Aid/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FisheriesComponent } from './fisheries/fisheries.component';
+import { PlantComponent } from './plant/plant.component';
+import { AgricultureComponent } from './agriculture/agriculture.component';
+import { VeterinaryComponent } from './veterinary/veterinary.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'fisheries', component:FisheriesComponent}
+  {path: '', component:HomeComponent},
+  {path: 'fisheries', component:FisheriesComponent},
+  {path: 'plant', component:PlantComponent},
+  {path: 'agriculture', component:AgricultureComponent},
+  {path: 'veterinary', component:VeterinaryComponent}
 ];
 
 @NgModule({
