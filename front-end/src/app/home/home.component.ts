4519d0a61cddc573e4bfdfe7277bc9c23d9eb784
1804054Miraz <u1804054@student.cuet.ac.bd>
2024-01-19 19:52:51 +0600

    friday te add korlam

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -27,15 +27,35 @@ export class HomeComponent {
 
   images = [
     {
-      url: 'assets/images/slider_bg02.jpg',
+      url: '/assets/images/pick_up.jpg',
       title: 'Farming',
       description: 'Farming description',
     },
     {
-      url: 'assets/images/slider_bg03.jpg',
+      url: 'https://media.istockphoto.com/id/855504838/photo/agriculture-on-rooftop.jpg?s=612x612&w=0&k=20&c=Lp2ARf2hv3aXuinUZeace5mg8OH4Eh3dhOB9BFR41wc=',
       title: 'Planting',
       description: 'Plating description',
     },
+    {
+      url: 'https://brooklyneagle.com/wp-content/uploads/2015/07/brooklyn-grange-garden.jpg',
+      title: 'Planting',
+      description: 'Plating description',
+    },
+    {
+      url: 'https://eadn-wc01-4177395.nxedge.io/wp-content/uploads/2020/05/69174846_10156662727869503_8266423507643006976_o.jpg',
+      title: 'Flower',
+      description: 'Plating description',
+    },
+    {
+      url: 'https://eadn-wc01-4177395.nxedge.io/wp-content/uploads/2020/05/MattFarming-1.jpg',
+      title: 'Planting',
+      description: 'Plating description',
+    },
+    {
+      url: 'https://eadn-wc01-4177395.nxedge.io/wp-content/uploads/2020/05/71113341_10156742812044503_1916056714008854528_o.jpg',
+      title: 'Planting',
+      description: 'Plating description',
+    }
   ];
 
   constructor(private router: Router) {}
